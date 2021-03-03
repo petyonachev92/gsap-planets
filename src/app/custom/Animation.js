32d72cc624ddfe8abef02f96f81764874c9c83d7
Petyo <pyo@abv.bg>
2021-03-03 13:14:29 +0200

    Added tweens id

--- src/app/custom/Animation.js
+++ src/app/custom/Animation.js
@@ -12,17 +12,20 @@ export default class Animation {
     async start() {
         this._scaleBtn.addEventListener("click", () => {
             this._tl.restart().clear()
-            this._tl.fromTo(this._planets, {scale: 1}, {scale: 0, stagger: 0.1})
+            this._tl.to(this._planets, {scale: 0, stagger: 0.1, id: "scaleStagger"})
             .to(this._planets, {scale: 1, stagger: 0.1}).repeat(-1)
+            console.log(this._tl.getTweensOf())
         })
         this._positionBtn.addEventListener("click", () => {
             this._tl.restart().clear()
-            this._tl.to(this._planets, {y: 50, stagger: {
+            this._tl.to(this._planets, {id: "positionStagger", y: 50, stagger: {
                 each: 0.05,
                 from: "center"}})
             .to(this._planets, {y: 0, stagger: {
                 each: 0.05,
-                from: "center"}}).repeat(-1)
+                from: "center"}}).repeat(-1);
+            
+            console.log(this._tl.getTweensOf())
         })
         this._stopBtn.addEventListener("click", async () => {
             this._tl.restart().clear().pause()
